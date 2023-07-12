8badde863fcbd1f50ac6db42df20c80cbce89d78
francisputong <francisputong01@gmail.com>
2023-07-12 21:52:31 +0800

    add routing and initial styling

--- app/sheet/[sheetId]/entry/index.tsx
+++ app/sheet/[sheetId]/entry/index.tsx
@@ -1,15 +1,34 @@
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import React from 'react';
+import { View } from 'react-native';
+import styles from './entry.style';
+import Typography from '../../../../components/common/Typography/Typography';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 type Props = {};
 
 const Entry = (props: Props) => {
+    const router = useRouter();
+
     return (
-        <Stack.Screen
-            options={{
-                headerTitle: 'Entry',
-            }}
-        />
+        <View style={styles.container}>
+            <Stack.Screen
+                options={{
+                    headerTitle: () => (
+                        <Typography weight='Bold'>
+                            Create Expense Log
+                        </Typography>
+                    ),
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={() => router.back()}>
+                            <Typography weight='Medium' color='primary'>
+                                Cancel
+                            </Typography>
+                        </TouchableOpacity>
+                    ),
+                }}
+            />
+        </View>
     );
 };
 
